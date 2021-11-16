66e494fc59ac3db253e7392842c5c7466ec5e987
Iván <ivanantonsagastizabal@gmail.com>
2021-11-16 14:13:36 +0100

    Improved js option permormance
    Signed-off-by: Iván <ivanantonsagastizabal@gmail.com>

--- detalle.php
+++ detalle.php
@@ -5,10 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalle de prenda</title>
+    <script src="js/motor.js"></script>
 </head>
-<body>
+<body id="detalle1">
     <h1>Estás en detalle</h1>
+    <a href="index.html">volver</a>
+    <div id="resultado1">&nbsp;
 
+</div>
     
 
 </body>
--- js/motor.js
+++ js/motor.js
@@ -67,23 +67,63 @@ window.addEventListener("load", function(event) {
     /*
     null --> null
      */
-    const result1 = document.getElementById("resultado1");
-    const search = document.getElementById("campo1");
-    cargarJSONOnLoad(result1);
-    //Keyup implica que cada vez que el usuario esté escribiendo y levante una tecla salta el evento
-    search.addEventListener("keyup", function() {
-        /*Cogemos cada vez que salte el evento el contenido del campo, para simular la lectura en tiempo real
-        de la barra de busqueda*/
-        const search = document.getElementById("campo1").value.toLowerCase();
-        if (search === "") {
-            cargarJSONOnLoad(result1)
-        } else {
-            //Pasamos el texto de busqueda mediante la funcion ignoreAccents(String)
-            cargarJSON1(result1, ignoreAccents(search));
-        }
+    const body1 = document.body;
+    if (body1.id == "index1") {
+        const result1 = document.getElementById("resultado1");
+        const search = document.getElementById("campo1");
+        cargarJSONOnLoad(result1);
+        //Keyup implica que cada vez que el usuario esté escribiendo y levante una tecla salta el evento
+        search.addEventListener("keyup", function() {
+            /*Cogemos cada vez que salte el evento el contenido del campo, para simular la lectura en tiempo real
+            de la barra de busqueda*/
+            const search = document.getElementById("campo1").value.toLowerCase();
+            if (search === "") {
+                cargarJSONOnLoad(result1)
+            } else {
+                //Pasamos el texto de busqueda mediante la funcion ignoreAccents(String)
+                cargarJSON1(result1, ignoreAccents(search));
+            }
 
-    })
+        })
+    } else if (body1.id == "detalle1") {
+        const result1 = document.getElementById("resultado1");
+        cargarDetalleJSON(this.id, result1);
+    }
+
+
 })
+
+function cargarDetalleJSON(div, id) {
+    /*
+    Div, int --> none
+    la función recibe el div objetivo donde cargar el html y el id de la prenda a mostrar
+    */
+    var ajax1 = new XMLHttpRequest();
+    ajax1.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj0 = this.responseText;
+            var obj1 = JSON.parse(obj0);
+            var obj2 = obj1["datos"];
+
+            let resultado = "";
+            for (let index = 0; index < obj2.length; index++) {
+                /*
+                Para todos los objetos que encontremos en json generaremos un valor en resultado
+                con el código html para inyectarlo en la página después y ahorrar los tiempos de carga
+                */
+                if (obj2[index].ide == id) {
+                    resultado += "<div><a href='detalle.php?ide=" + obj2[index].ide + "'><div><img class='imagen' src='" + obj2[index].ruta + "'></div><div><h3 class='nombre'>" + obj2[index].articulo + "</h3></div><div><p class='dinero'>" + obj2[index].pvp + "€</p></div></a></div>";
+                }
+
+
+            }
+            //Incorporamos a nuestro HTML los resultados del json
+            document.getElementById(div.id).innerHTML = resultado;
+        }
+    };
+    ajax1.open("GET", "json/db1.json");
+    ajax1.send();
+}
 
 function ignoreAccents(text) {
     /*
